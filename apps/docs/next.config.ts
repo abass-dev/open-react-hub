f3139fbb85ece5a80170be5103fa156418752899
Abass Dev <abassthedev@gmail.com>
2025-01-21 01:34:45 +0100

    doc first deployment

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@open-react-hub/*"]
+  transpilePackages: [
+    '@open-react-hub/button',
+    '@open-react-hub/cli',
+    '@open-react-hub/split-text',
+    '@open-react-hub/code-block',
+    '@open-react-hub/core'
+  ],
 };
 
 export default nextConfig;
